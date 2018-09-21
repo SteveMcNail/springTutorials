3c8054a0496145d8a4b6a28f42f0076545d370cf
Stephan Naegele <stephan.naegele@pm.me>
2018-09-21 16:07:49 +0200

    Bean lifecycle methods with Annotations

--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -2,11 +2,12 @@ package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
 
   private FortuneService fortuneService;
@@ -19,6 +20,20 @@ public class TennisCoach implements Coach {
   public TennisCoach() {
     System.out.println("\n>> TennisCoach default constructor");
   }
+
+
+  // define init method
+  @PostConstruct
+  public void doMyStartupStuff() {
+    System.out.println(">> TennisCoach: is doing some startup-stuff, like polishing the tennis balls");
+  }
+
+  // define destroy method
+  @PreDestroy
+  public void doMyCleanupStuff() {
+    System.out.println(">> TennisCoach: is doing some cleanup-stuff, like raking the sand");
+  }
+
 
   @Override
   public String getDailyWorkout() {
