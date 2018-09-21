b31500089813e8961e46c518361de195ae24c3b8
Stephan Naegele <stephan.naegele@pm.me>
2018-09-21 17:12:58 +0200

    loading Property-files into java configuration

--- spring-demo-annotations/src/com/luv2code/springdemo/SwimJavaConfigDemoApp.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/SwimJavaConfigDemoApp.java
@@ -9,11 +9,13 @@ public class SwimJavaConfigDemoApp {
     AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
 
     // retrieve bean from sprint container
-    Coach theTennisCoach = context.getBean("swimCoach", Coach.class);
+    SwimCoach theSwimmCoach = context.getBean("swimCoach", SwimCoach.class);
 
     // do something
-    System.out.println("\nThe tennis coach mumbles " + theTennisCoach.getDailyWorkout());
-    System.out.println("\nThe tennis coach looks into his crystall ball and proclaims '" + theTennisCoach.getDailyFortune() + "'");
+    System.out.println("\nThe tennis coach mumbles " + theSwimmCoach.getDailyWorkout());
+    System.out.println("\nThe tennis coach looks into his crystall ball and proclaims '" + theSwimmCoach.getDailyFortune() + "'");
+    System.out.println("\nhe shouts my email is " + theSwimmCoach.getEmail());
+    System.out.println("\nand my team is " + theSwimmCoach.getTeam());
 
     // close context
     context.close();
