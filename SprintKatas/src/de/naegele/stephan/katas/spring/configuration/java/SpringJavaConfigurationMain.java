248069f37808133ba769d24d26036f9d4dcd2a05
Stephan Naegele <stephan.naegele@pm.me>
2018-09-26 11:07:35 +0200

    präzisierung

--- SprintKatas/src/de/naegele/stephan/katas/spring/configuration/java/SpringJavaConfigurationMain.java
+++ SprintKatas/src/de/naegele/stephan/katas/spring/configuration/java/SpringJavaConfigurationMain.java
@@ -4,7 +4,7 @@ public class SpringJavaConfigurationMain {
 
   public  static final void main (String[] args) {
 
-    // TODO: Schreibe eine FortuneService implementierung, die hardcoded eine "Weisheit" ausgibt.
+    // TODO: Schreibe eine FortuneService-Implementierung (TennisFortuneService), die hardcoded eine "Weisheit" ausgibt.
     // TODO: Implementiere den Coach aus, so dass er einen hardcoded Workout ausgibt, die neue FortuneServiceImplementierung verwendet und deren Ausgabe verwendet
     // TODO: Erstelle eine Spring-Konfiguration  (SportConfig) via Java und wende Inversion of Controll und Dependency Injection auf die entsprechenden Klassen an.
     // TODO: Implementiere die MainKlasse aus und verwende die entsprechenden Klassen/gebe via System.out.println die Anweisungen/Weisheiten aus.
--- SprintKatas/src/de/naegele/stephan/katas/spring/configuration/xml/SpringXmlConfigurationMain.java
+++ SprintKatas/src/de/naegele/stephan/katas/spring/configuration/xml/SpringXmlConfigurationMain.java
@@ -3,7 +3,7 @@ package de.naegele.stephan.katas.spring.configuration.xml;
 public class SpringXmlConfigurationMain {
 
   public final static void main (String[] args) {
-    // TODO: Schreibe eine FortuneService implementierung, die hardcoded eine "Weisheit" ausgibt.
+    // TODO: Schreibe eine FortuneService-Implementierung (TennisFortuneService), die hardcoded eine "Weisheit" ausgibt.
     // TODO: Implementiere den Coach aus, so dass er einen hardcoded Workout ausgibt, die neue FortuneServiceImplementierung verwendet und deren Ausgabe verwendet
     // TODO: Erstelle eine Spring-Konfiguration via XML (SportConfig) und wende Inversion of Controll und Dependency Injection auf die entsprechenden Klassen an.
     // TODO: Implementiere die MainKlasse aus und verwende die entsprechenden Klassen/gebe via System.out.println die Anweisungen/Weisheiten aus.
