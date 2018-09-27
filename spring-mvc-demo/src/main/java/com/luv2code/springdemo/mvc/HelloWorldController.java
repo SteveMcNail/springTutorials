8414c0b21357ebd5f32ad2c69aca00279e88f1f9
Stephan Naegele <stephan.naegele@pm.me>
2018-09-27 09:42:44 +0200

    create ambigious mapping - error

--- spring-mvc-demo/src/main/java/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/main/java/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
-@RequestMapping("/helloWorld")
 public class HelloWorldController {
 
   // show inital form with controller method
